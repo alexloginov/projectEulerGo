f98d7593431b01dfb36f6e0fe53ba6d6f8accdf5
Alexander Loginov <alex@loginov.pro>
2014-02-16 22:54:35 +0000

    problem #4 solved

--- src/problem4.go
+++ src/problem4.go
@@ -6,10 +6,27 @@ import (
 )
 
 func main() {
+	max:=0
+	for i:=999; i>=100; i--{
+		for j:=999; j>=100; j--{
+			val := i*j
+			if isPalindrome(strconv.Itoa(i*j)) && max<val{
+				max = val
+			}
+		}
+	}
 
+	fmt.Printf("Result: %d",max)
 }
 
-func isPalindrome(n int) bool{
-	num:=strconv.Itoa(n)
+func isPalindrome(p string) bool{
+	s := []byte(p)
+	j := len(s)-1
+	for i:=0;i<len(s)/2;i++{
+		if s[i] != s[j]{
+			return false
+		}
+		j--
+	}
+	return true
 }
-
